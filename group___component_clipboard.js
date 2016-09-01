9c719f39581ba391ac102b8cb6a84b51cb09b0df
Wanzyee <wanzyeew@gmail.com>
2016-09-02 01:45:32 +0800

    Add files via upload

---
+++ group___component_clipboard.js
@@ -0,0 +1,36 @@
+var group___component_clipboard =
+[
+    [ "Clipboard", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_clipboard.html", [
+      [ "OpenWindow", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_clipboard.html#a513b88d38998ec5b3a1b82fbc73afc9c", null ],
+      [ "IsCopyable", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_clipboard.html#a0520ff90731a357642e64d938d4d0589", null ],
+      [ "Copy", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_clipboard.html#a33e4b97b0a120a409d33a32cc493af1f", null ],
+      [ "Clear", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_clipboard.html#a8385b0d378201173be1357256ef356eb", null ],
+      [ "AddItemsToMenu", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_clipboard.html#a839e9dda1fdb8aaed2b2c2582fa1254e", null ]
+    ] ],
+    [ "MaterialPropertyCopier", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_material_property_copier.html", [
+      [ "Copy", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_material_property_copier.html#ab29075c8ca08ab5ed8f0731674da4002", null ],
+      [ "Paste", "class_wanzyee_studio_1_1_editrix_1_1_toolkit_1_1_material_property_copier.html#afae0217cbb3f536ac6feb3c21ae2073e", null ]
+    ] ],
+    [ "AuxCursor", "class_wanzyee_studio_1_1_editrix_1_1_aux_cursor.html", [
+      [ "mode", "class_wanzyee_studio_1_1_editrix_1_1_aux_cursor.html#a7c6772c8f606b6ac20ecd99ba858a63e", null ],
+      [ "icon", "class_wanzyee_studio_1_1_editrix_1_1_aux_cursor.html#a9cd01d1cde51abbeba6afac87f7b3576", null ],
+      [ "offset", "class_wanzyee_studio_1_1_editrix_1_1_aux_cursor.html#a05a86d9e3d6f4daa58feabb0351c15a8", null ],
+      [ "size", "class_wanzyee_studio_1_1_editrix_1_1_aux_cursor.html#aef1d073726fc4b857570fbae3230b8dd", null ]
+    ] ],
+    [ "EditrixStyle", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html", [
+      [ "CreateTexture", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#a5efd4ca473699b9e372bb965b95e6db4", null ],
+      [ "CreateIcon", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#a760a957853f0d90aaefc40dba04d6164", null ],
+      [ "CreateCursor", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#ab81f1a1e10be618b4bfc0d1cefc95cbb", null ],
+      [ "splitterPixel", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#aa35ecf15cd244e96dec6cf0d21ef89d8", null ],
+      [ "statusBar", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#ae4358dbdc23372131882726541eff46f", null ],
+      [ "questionIcon", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#afa45ca183b22145713a46c7af98ce763", null ],
+      [ "hierarchyIcon", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#ab737c95489a39be23f3185b4a48d2646", null ],
+      [ "copyIcon", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#ac0fe68d9478ca2bd7066a7c198a19436", null ],
+      [ "aimIcon", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#ac11b691c2ce5059673c02130ec766839", null ],
+      [ "clipboardIcon", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#a49fba5b9a101e3c5f920c4a4e17def9d", null ],
+      [ "stopCursor", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#ab6d45d275e7bd2bce0d3b45b3b872983", null ],
+      [ "copyCursor", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#aef7396942c457397051476cc609c99ea", null ],
+      [ "linkCursor", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#ade56500cdc624c1da2a3d6d2810c4389", null ],
+      [ "moveCursor", "class_wanzyee_studio_1_1_editrix_1_1_editrix_style.html#aadfeba17b079a57388d20a8e3ce0a32f", null ]
+    ] ]
+];
