fd3ca1dd216cd6f0a59160b4d332c2c25dfac95c
Wanzyee <wanzyeew@gmail.com>
2017-03-26 20:45:01 +0800

    upd doc

--- html/group___released.js
+++ html/group___released.js
@@ -1,10 +1,10 @@
 var group___released =
 [
     [ "Clipboard", "group___clipboard.html", "group___clipboard" ],
-    [ "Event Delegator", "group___event_delegator.html", "group___event_delegator" ],
     [ "3P Maker", "group___p_maker.html", "group___p_maker" ],
+    [ "Event Delegator", "group___event_delegator.html", "group___event_delegator" ],
     [ "Www Loader", "group___www_loader.html", "group___www_loader" ],
-    [ "Singleton Liberation", "group___singleton_liberation.html", "group___singleton_liberation" ],
+    [ "Editor Aider", "group___editor_aider.html", "group___editor_aider" ],
     [ "Json.NET Converters", "group___json_net_converters.html", "group___json_net_converters" ],
-    [ "Editor Aider", "group___editor_aider.html", "group___editor_aider" ]
+    [ "Singleton Liberation", "group___singleton_liberation.html", "group___singleton_liberation" ]
 ];
